eedc531addcecb1811c23c52b70068e7e2f39d88
James <jyang1737@gmail.com>
2018-12-01 18:21:10 -0500

    began insert data

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -3,6 +3,9 @@ package Application;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+import java.io.*;
 
 import static Application.SQLBase.getConnection;
 
@@ -12,6 +15,32 @@ public class Main {
         Connection con = getConnection("epic_auto");
         Interface ui = new Interface(con);
 
-        //ResultSet rs = executeQuery(
+        PreparedStatement insertBrand = null;
+        PreparedStatement insertCustomer = null;
+        PreparedStatement insertDealer = null;
+        PreparedStatement insertInventory = null;
+        PreparedStatement insertModel = null;
+        PreparedStatement insertSale = null;
+        PreparedStatement insertVehicle = null;
+
+        
+        try {
+            con.setAutoCommit(false);
+
+            File file = new File("data-gen/brand.csv");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+            while ((st = br.readLine()) != null) {
+                String[] parts = st.split(",");
+                for (String a: parts)
+                    System.out.println(a);
+                
+                String addBrand = "insert into brand(brandname, country) values" + "(?,?)";
+                insertBrand = con.prepareStatement(addBrand);
+                
+            }
+        } catch(Exception e) {
+
+        }
     }
 }
